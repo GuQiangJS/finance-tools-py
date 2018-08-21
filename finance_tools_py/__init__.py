0906a6fd81352756e62e814e841f59362e364432
GuQiangJS <GuQiangJS@hotmail.com>
2018-08-21 21:35:23 +0800

    version

--- finance_tools_py/__init__.py
+++ finance_tools_py/__init__.py
@@ -1,2 +1,7 @@
 # Copyright (C) 2018 GuQiangJs.
 # Licensed under Apache License 2.0 <see LICENSE file>
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
