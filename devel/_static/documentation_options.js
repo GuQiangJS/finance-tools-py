6ea2ff3cfa0c92bc215f15175499359256b671b4
Travis CI User <travis@example.org>
2018-08-23 03:00:20 +0000

    Update docs after building Travis build 26 of GuQiangJS/finance-tools-py
    The docs were built from the branch 'master' against the commit
    d5a999f41fd1617b33d2eda241147f6475a6866b.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/GuQiangJS/finance-tools-py/jobs/419465616.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.30.g6931357',
+    VERSION: '0+untagged.31.gd5a999f',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
