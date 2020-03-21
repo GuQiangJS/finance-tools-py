be9619acf6450076a2ee954d1b6a998d793d8226
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-03-21 13:35:52 +0000

    Update docs after building Travis build 134 of GuQiangJS/finance-tools-py
    The docs were built from the branch 'master' against the commit
    314a5886acef0c039d6e7cb1dfa2410ce79ef75e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/GuQiangJS/finance-tools-py/jobs/665180387.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.19+43.g7e352ff',
+    VERSION: '0.0.19+35.g314a588',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
